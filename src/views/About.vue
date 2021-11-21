ad9f9396464ea55097739f991d1a5ba1d141fbfc
Pierre-Yves Gicquel <pierre-yves.gicquel@hds.utc.fr>
2021-11-21 21:30:01 +0100

    Many change

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="p-grid p-text-left">
+      <div class="p-col-5">
+<Card class="p-shadow-4">
+    <template #title>
+        About CASPOA Experiment
+    </template>
+    <template #content>
+      This software has been conceived in the framework of the <b>ORCHESTRAA project</b> for the training of air force officer in stress situation. 
+      This software has been conceived at the Heudiasyc laboratory under the direction of Domitile Lourdeaux. The main contributor for this software is Pierre-Yves Gicquel.
+      <br>
+      <br>
+      This is a research software and should not be used in an educationnal goal.
+    </template>
+      <template #footer>
+       The code for this software is accessible at :
+        
+    </template>
+</Card>
+      </div></div>
 </template>
+<script>
+import Card from 'primevue/card'
+export default {
+  components:{
+    Card
+  }
+  
+}
+</script>
